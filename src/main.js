583dca0ed2a08134564070b7e0938877871f4798
oshadian <oshadian@ucsc.edu>
2021-04-29 14:21:42 -0700

    separated assets into files

---
+++ src/main.js
@@ -0,0 +1,21 @@
+/**
+ * Kitty Run
+ * Oran Shadian
+ * 
+ * 
+ * 
+ * 
+ */
+
+// config
+let config = {
+    type: Phaser.CANVAS,
+    width: 640,
+    height: 480,
+    scene: [Play, Menu, Instruction],
+}
+
+// game variable
+let game = new Phaser.Game(config);
+
+// 
