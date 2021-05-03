ffb11c7113d9e815053ac0be2953ba7a61a4be9b
oshadian <oshadian@ucsc.edu>
2021-05-03 15:13:06 -0700

    added keyR to main

--- src/main.js
+++ src/main.js
@@ -23,7 +23,7 @@ let borderUISize = game.config.height / 5;
 let borderPadding = borderUISize / 3;
 
 // keyboard input
-let keySPACE, keyP, keyW, keyUP, keyDOWN;
+let keySPACE, keyP, keyW, keyR, keyUP, keyDOWN;
 
 // variables needed for game
 let lives = 9, score = 0, obstacles = [], pointObjects = [], tables = [], frames = [], indexCount = 0, makingObstacle = false, makingPoint = false, obsDelay, pointDelay, runningTime;
