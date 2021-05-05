f066e5b7ee37e36dad7d8b54dc1b82a4317efdb0
oshadian <oshadian@ucsc.edu>
2021-05-05 11:35:34 -0700

    added creative tilt in main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 /**
  * Kitty Run
- * Oran Shadian
  * 
+ * Our design team, Gabriella Flores and Makkenzie Zepeda, formalized the game's style by doing outside studies of the cult classic cartoon, Tom and Jerry. 
+ * The creative influence is notable in the simplistic lineart and color palette used throughout the props, main characters, and backdrops. 
+ * The design goal was a stylized cartoon experience. All the music was made with real instruments and sounds by Makkenzie Zepeda and her boyfriend Dominic Castelli (sound designer). 
+ * The music was designed to enhance the cat chase, the doom of a cat bath, and the jazzy relaxation state of a sleeping cat.
  * 
- * 
+ * For programming, Oran made some interesting choices for how to spawn new objects (both obstacles and points).
+ * Oran created a function that takes 2 numbers and another function as an argument, and calls it with a delay between
+ * the two numbers that were sent. Another interesting feature is how the objects change to their shattered form then
+ * get restored when they appear again, Oran accomplished this by storing a copy of the texture when the object is constructed
+ * and when the reset function for the object is called, it restores the original texture. (Oran: looking back, I would have done things a little diffrently,
+ * but I made it work with the knowledge I had). 
  * 
  */
 
